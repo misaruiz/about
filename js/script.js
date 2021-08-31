4d14ecca6769ae4f72e2580aca2bd8a05725228d
misaruiz <misaruiz@gmail.com>
2021-08-30 22:03:40 -0700

    more updates

--- js/script.js
+++ js/script.js
@@ -2,9 +2,13 @@
  * Work Experience
  */
 
+const companies = {
+    tsia: 'Technology & Services Industry Association (TSIA)', 
+    hme: 'HM Electronics, Inc. (HME)'};
+
 const srUXTSIA = new Jobs(
-    'Senior UI/UX Designer',
-    'Technology & Services Industry Association (TSIA)',
+    `Senior <abbr title="User Interface / User Experience" data-bs-toggle="tooltip" data-bs-placement="bottom">UI/UX</abbr> Designer`,
+    companies.tsia,
     'Feb 2020',
     'Present',
     'San Diego',
@@ -15,7 +19,7 @@ const srUXTSIA = new Jobs(
 
 const srWebTSIA = new Jobs(
     'Senior Web Designer',
-    'Technology & Services Industry Association (TSIA)',
+    companies.tsia,
     'Nov 2018',
     'Feb 2020',
     'San Diego',
@@ -27,7 +31,7 @@ const srWebTSIA = new Jobs(
 
 const graphicTSIA = new Jobs(
     'Graphic Designer',
-    'Technology & Services Industry Association (TSIA)',
+    companies.tsia,
     'Feb 2017',
     'Nov 2018',
     'San Diego',
@@ -38,7 +42,7 @@ const graphicTSIA = new Jobs(
 
 const graphicHME = new Jobs(
     'Graphic Designer',
-    'HM Electronics, Inc. (HME)',
+    companies.hme,
     'Jan 2010',
     'Feb 2017',
     'Poway',
@@ -80,14 +84,29 @@ const extra = new Sidebar('Extra', 'geo-alt', [
 /**
  * Contact Info
  */
-const name = 'Misael Ruiz';
-const title = 'UX Designer';
-const website = new ContactInfo('misaruiz.github.io', 'https://misaruiz.github.io/', 'globe2');
-const email = new ContactInfo('hello@misaruiz.com', 'mailto:hello@misaruiz.com', 'envelope');
-const phone = new ContactInfo('619.292.1438', 'tel:619-292-1438', 'telephone');
+const myName = 'Misael Ruiz';
+const myTitle = 'UX Designer';
+const myAvatar = 'assets/misaruiz-avatar.png';
+const myWebsite = new ContactInfo('misaruiz.github.io', 'https://misaruiz.github.io/', 'globe2');
+const myEmail = new ContactInfo('hello@misaruiz.com', 'mailto:hello@misaruiz.com', 'envelope');
+const myPhone = new ContactInfo('619.292.1438', 'tel:619-292-1438', 'telephone');
 
+document.querySelector('title').innerHTML = `${myName} - ${myTitle}`;
+document.querySelector('a.navbar-brand').insertAdjacentHTML("beforeend", `<strong>${myName}</strong> - ${myTitle}`);
+document.querySelectorAll('img.myAvatar').forEach(img => img.setAttribute('src', myAvatar));
 
 /**
  * Gets current year
  */
  document.getElementById("year").innerHTML = new Date().getFullYear();
+
+
+ /**
+  * Initialize Tooltips
+  */
+  window.onload = function() {
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+}
